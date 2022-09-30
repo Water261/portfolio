3c55e037c684306bc1114d2984c05d40e83a4c73
Water261 <water261@outlook.com.au>
2022-09-30 15:47:37 +1000

    feat: Wrap navbar in container

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -3,23 +3,25 @@ use yew::prelude::*;
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
 	html! {
-		<nav class={"navbar"} role={"navigation"} aria-label={"main navigation"}>
-			<div class={"navbar-brand"}>
-				<a class={"navbar-item"}>
-					{"Icon"}
-				</a>
-			</div>
-
-			<div class={"navbar-menu"}>
-				<div class={"navbar-end"}>
-					<a href={"#projects"} class={"navbar-item"}>
-						{"Projects"}
-					</a>
-					<a href={"#about"} class={"navbar-item"}>
-						{"About"}
+		<div class={"container is-fluid"}>
+			<nav class={"navbar"} role={"navigation"} aria-label={"main navigation"}>
+				<div class={"navbar-brand"}>
+					<a class={"navbar-item"}>
+						{"Icon"}
 					</a>
 				</div>
-			</div>
-		</nav>
+
+				<div class={"navbar-menu"}>
+					<div class={"navbar-end"}>
+						<a href={"#projects"} class={"navbar-item"}>
+							{"Projects"}
+						</a>
+						<a href={"#about"} class={"navbar-item"}>
+							{"About"}
+						</a>
+					</div>
+				</div>
+			</nav>
+		</div>
 	}
 }
