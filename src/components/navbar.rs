74ac10c85730351b3d4874d3761793116097bb08
Water261 <water261@outlook.com.au>
2022-10-01 08:16:13 +1000

    fix: Change the order of nav links to fit page layout

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -2,24 +2,24 @@ use yew::prelude::*;
 
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
-	html! {
-		<nav class={"navbar is-fixed-top px-4 is-primary"} role={"navigation"} aria-label={"main navigation"}>
-			<div class={"navbar-brand"}>
-				<a href={"#"} class={"navbar-item"}>
-					<span class="material-symbols-outlined">{"home"}</span>
-				</a>
-			</div>
+    html! {
+        <nav class={"navbar is-fixed-top px-4 is-primary"} role={"navigation"} aria-label={"main navigation"}>
+            <div class={"navbar-brand"}>
+                <a href={"#"} class={"navbar-item"}>
+                    <span class="material-symbols-outlined">{"home"}</span>
+                </a>
+            </div>
 
-			<div class={"navbar-menu"}>
-				<div class={"navbar-end"}>
-					<a href={"#projects"} class={"navbar-item"}>
-						{"Projects"}
-					</a>
-					<a href={"#about"} class={"navbar-item"}>
+            <div class={"navbar-menu"}>
+                <div class={"navbar-end"}>
+                	<a href={"#about"} class={"navbar-item"}>
 						{"About"}
 					</a>
-				</div>
-			</div>
-		</nav>
-	}
+                    <a href={"#projects"} class={"navbar-item"}>
+                        {"Projects"}
+                    </a>
+                </div>
+            </div>
+        </nav>
+    }
 }
