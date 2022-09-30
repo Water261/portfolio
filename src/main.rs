b7e64d811695396230721040c30458df97415911
Water261 <water261@outlook.com.au>
2022-09-30 15:13:31 +1000

    feat: Add yew

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <></>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
