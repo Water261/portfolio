b150c41a8632b1dc12ae3767077bd54ac8ead1d5
Water261 <water261@outlook.com.au>
2022-10-01 08:13:09 +1000

    feat: General cleanup plus longer descriptions

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -9,28 +9,21 @@ pub fn projects() -> Html {
                 {"Projects"}
             </h1>
             <div class={"tile is-ancestor"}>
-                <div class={"tile is-parent is-4"}>
+                <div class={"tile is-parent is-6"}>
                     <Project
                         project_name={"ruhroh"}
                         project_repo={"https://github.com/Water261/ruhroh"}
-                        project_desc={"A logitech device manager"}
+                        project_desc={"An unofficial device manager for Logitech HID++ devices."}
                         project_languages={vec![String::from("Rust")]}
                     />
                 </div>
-                <div class={"tile is-parent is-4"}>
+                <div class={"tile is-parent is-6"}>
                 	<Project
                         project_name={"portfolio"}
                         project_repo={"https://github.com/Water261/portfolio"}
-                        project_desc={"My portfolio website"}
+                        project_desc={"My portfolio website, containing the source code for the application you see here right now."}
                         project_languages={vec![String::from("Rust"), String::from("HTML")]}
                     />
-				</div>
-                <div class={"tile is-parent is-4"}>
-                	<div class={"tile box is-child"}>
-						<h1 class={"title"}>
-							{"Three"}
-						</h1>
-					</div>
 				</div>
             </div>
         </section>
