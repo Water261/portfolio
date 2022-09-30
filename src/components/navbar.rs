ff32cdc614ba7e1b4c217f1f9adab6043cb7bdf7
Water261 <water261@outlook.com.au>
2022-09-30 16:34:04 +1000

    feat: Change to a fixed navbar with primary background

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -3,25 +3,23 @@ use yew::prelude::*;
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
 	html! {
-		<div class={"container is-fluid"}>
-			<nav class={"navbar"} role={"navigation"} aria-label={"main navigation"}>
-				<div class={"navbar-brand"}>
-					<a href={"#"} class={"navbar-item"}>
-						<img class={"image is-24x24"} src={"https://via.placeholder.com/24.png"} />
+		<nav class={"navbar is-fixed-top px-4 is-primary"} role={"navigation"} aria-label={"main navigation"}>
+			<div class={"navbar-brand"}>
+				<a href={"#"} class={"navbar-item"}>
+					<img class={"image is-24x24"} src={"https://via.placeholder.com/24.png"} />
+				</a>
+			</div>
+
+			<div class={"navbar-menu"}>
+				<div class={"navbar-end"}>
+					<a href={"#projects"} class={"navbar-item"}>
+						{"Projects"}
+					</a>
+					<a href={"#about"} class={"navbar-item"}>
+						{"About"}
 					</a>
 				</div>
-
-				<div class={"navbar-menu"}>
-					<div class={"navbar-end"}>
-						<a href={"#projects"} class={"navbar-item"}>
-							{"Projects"}
-						</a>
-						<a href={"#about"} class={"navbar-item"}>
-							{"About"}
-						</a>
-					</div>
-				</div>
-			</nav>
-		</div>
+			</div>
+		</nav>
 	}
 }
