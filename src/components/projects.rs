1ab367f8f1e668e9cd9f7f3aaabb08f8150d1db1
Water261 <water261@outlook.com.au>
2022-09-30 16:08:00 +1000

    feat: Add dummy projects

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -2,11 +2,34 @@ use yew::prelude::*;
 
 #[function_component(Projects)]
 pub fn projects() -> Html {
-	html! {
-		<section id={"projects"} class={"section is-medium"}>
-			<h1 class={"title"}>
-				{"Projects"}
-			</h1>
-		</section>
-	}
+    html! {
+        <section id={"projects"} class={"section is-medium"}>
+            <h1 class={"title"}>
+                {"Projects"}
+            </h1>
+            <div class={"tile is-ancestor"}>
+                <div class={"tile is-parent is-4"}>
+                    <div class={"tile box is-child"}>
+                        <h1 class={"title"}>
+                            {"One"}
+                        </h1>
+                    </div>
+                </div>
+                <div class={"tile is-parent is-4"}>
+                	<div class={"tile box is-child"}>
+						<h1 class={"title"}>
+							{"Two"}
+						</h1>
+					</div>
+				</div>
+                <div class={"tile is-parent is-4"}>
+                	<div class={"tile box is-child"}>
+						<h1 class={"title"}>
+							{"Three"}
+						</h1>
+					</div>
+				</div>
+            </div>
+        </section>
+    }
 }
