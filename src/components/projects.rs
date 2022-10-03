eff6e98763792292d5183169634d12a13b78e2b7
Water261 <water261@outlook.com.au>
2022-10-03 11:03:36 +1000

    feat: Update project descriptions

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -13,7 +13,7 @@ pub fn projects() -> Html {
                     <Project
                         project_name={"ruhroh"}
                         project_repo={"https://github.com/Water261/ruhroh"}
-                        project_desc={"An unofficial device manager for Logitech HID++ devices."}
+                        project_desc={"Built in Rust, ruhroh aims to be a complete replica of PixlOne's logiops project. Ruhroh is designed to handle Logitech devices through its simple configuration file written in JSON."}
                         project_languages={vec![String::from("Rust")]}
                     />
                 </div>
@@ -21,8 +21,8 @@ pub fn projects() -> Html {
                 	<Project
                         project_name={"portfolio"}
                         project_repo={"https://github.com/Water261/portfolio"}
-                        project_desc={"My portfolio website, containing the source code for the application you see here right now."}
-                        project_languages={vec![String::from("Rust"), String::from("HTML")]}
+                        project_desc={"Built with yew, portfolio is designed to serve as my personal portfolio while demonstrating the power of WebAssembly. It is designed to be easy to modify while still looking professional, portfolio achieves this through the use of the Bulma CSS framework."}
+                        project_languages={vec![String::from("Rust"), String::from("Yew")]}
                     />
 				</div>
             </div>
